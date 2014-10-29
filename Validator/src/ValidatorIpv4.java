eda22171a4dcb2ec14bbb20b62342bb613b7e0c3
Costin Nita <costin.nita@cti.pub.ro>
2014-10-29 19:09:52 +0200

    adds IPv4 validator

--- Validator/src/ValidatorIpv4.java
+++ Validator/src/ValidatorIpv4.java
@@ -1,4 +1,26 @@
+import java.util.*;
+import java.lang.NumberFormatException;
 
 public class ValidatorIpv4 {
+	
+	public static boolean isIPv4(String inputString) {
+		String[] tempString = inputString.split(".");
+		if (tempString.length == 4) {
+			for(int i = 0; i < tempString.length; i++) {
+				try {
+					if (Integer.parseInt(tempString[i]) <= 255 && Integer.parseInt(tempString[i]) >= 0) {
+						continue;
+					} else {
+						return false;
+					}
+				} catch (NumberFormatException e) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		return false;
+	}
 
 }
