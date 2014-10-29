6581300b9afd26e39f28848c96db74e9f0f7a794
Cristian Grigoras <grigoras.cristian92@gmail.com>
2014-10-29 19:36:51 +0200

    Fix ipv6 length

--- Validator/src/ValidatorIpv6.java
+++ Validator/src/ValidatorIpv6.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class ValidatorIpv6 {
 	public static boolean isIpv6(String address)
 	{
-		Pattern p = Pattern.compile("([0-9A-F]{4}:){11}[0-9A-F]{4}");
+		Pattern p = Pattern.compile("([0-9A-F]{4}:){7}[0-9A-F]{4}");
 //		Pattern p = Pattern.compile("([0-9A-F]{4}:){11}");
 		Matcher m = p.matcher(address);
 		
