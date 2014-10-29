53b10a779e3f99ce2c018dfd7483fbf6dbf85d96
mihaiilie14 <mihai.iliemihai@gmail.com>
2014-10-29 19:24:22 +0200

    Update ValidatorIpv6.java

--- Validator/src/ValidatorIpv6.java
+++ Validator/src/ValidatorIpv6.java
@@ -1,4 +1,14 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ValidatorIpv6 {
-
+	public static boolean isIpv6(String address)
+	{
+		Pattern p = Pattern.compile("([0-9A-F]{4}:){11}[0-9A-F]{4}");
+//		Pattern p = Pattern.compile("([0-9A-F]{4}:){11}");
+		Matcher m = p.matcher(address);
+		
+		return m.matches();
+	}
 }
